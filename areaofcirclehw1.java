c4aebf4b8f546d15b5b01d710dcbb8588ab5ad70
vishrut123vyas <139750431+vishrut123vyas@users.noreply.github.com>
2024-04-06 21:05:40 +0530

    Add files via upload

---
+++ areaofcirclehw1.java
@@ -0,0 +1,11 @@
+import java .util.*;
+
+public class areaofcirclehw1 {
+    public static void main(String[] args) {
+       float r= 10F;
+       final float PI=3.14F;
+       float area= PI*r*r;
+       System.out.println("The area of the circle is "+area);
+
+    }
+}
