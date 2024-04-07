12b0be43c82c5743c98e844c9f3c7c2a9257a1df
Vishrut Vyas <139750431+vishrut123vyas@users.noreply.github.com>
2024-04-07 23:59:24 +0530

    Add files via upload

---
+++ monthshw2.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class monthshw2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int months=sc.nextInt();
+        
+
+        // if (a+b==10) {
+        //     System.out.println("addition");
+        // } else if (a-b==12){
+        //     System.out.println("subtraction");
+        // } else if(a*b==20){
+        //     System.out.println("multiplication");
+        // }
+        // else{
+        //     System.out.println("remainder");
+// }
+  
+            switch (months) {
+                case 1:System.out.println("january");
+                break;
+                case 2: System.out.println("february");
+                break;
+                case 3: System.out.println("march");
+                break;
+                case 4: System.out.println("april");
+                break;
+                case 5: System.out.println("may");
+                break;
+                case 6:System.out.println("june");
+                break;
+                
+                case 7:System.out.println("july");
+                break;
+                
+                case 8:System.out.println("august");
+                break;
+                case 9:System.out.println("september");
+                break;
+                case 10:System.out.println("october");
+                break;
+                case 11:System.out.println("novenber");
+                break;
+                case 12:System.out.println("december");
+                break;
+                
+                
+                
+                
+                
+                
+                
+                default:System.out.println("nothing");
+            }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    }
+}
