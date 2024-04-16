9c9e00cfa18e7d841e91b5d3c03be1a349d1ede7
Vishrut Vyas <139750431+vishrut123vyas@users.noreply.github.com>
2024-04-16 22:54:59 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,18 @@
+// public class forloop {
+
+//     public static void main(String[] args) {
+//          for (int counter = 0; counter < 13; counter++) {
+//                System.out.println(counter+" ");
+//             }
+        
+//     }
+    
+// }
+public class forloop {
+
+    public static void main(String[] args) {
+        for (int counter = 0; counter < 13; counter++) {
+            System.out.println(counter + " ");
+        }
+    }
+}
