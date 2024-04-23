23a599db5de98a4f6ae7f4591a6a4b175e6a1d91
Vishrut Vyas <139750431+vishrut123vyas@users.noreply.github.com>
2024-04-23 23:48:37 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,10 @@
+public class dowhile {
+    public static void main(String[] args) {
+        int y=0;
+    do {
+        System.out.println("pawan putra HANUMAN");
+        y=y+1;
+    } while (y<50);
+    
+    }
+}
